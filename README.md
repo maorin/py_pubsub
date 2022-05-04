a95869c45074ff64ddab64742ee784aa229349f4
maorin <supermaojj@qq.com>
2022-05-04 13:48:25 +0800

    可以判断目标是否在瞄准点内了。 现在的问题是： 多个目标时需要先锁定一个目标。所以先判断出目标是否是之前的那一个。 这应该跟目标跟踪相关

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # py_pubsub
+https://docs.ros.org/en/rolling/Tutorials/Writing-A-Simple-Py-Publisher-And-Subscriber.html#
+
+ros2 run py_pubsub talker
+
+
+ros2 run py_pubsub listener
--- py_pubsub/subscriber_member_function.py
+++ py_pubsub/subscriber_member_function.py
@@ -16,9 +16,36 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+import os
+import numpy as np
+from chameleon.config import TRUE
 
 
+
+
+# 2336x1080 屏幕长宽
+# 1168x 540 中心点
+# 手动测试 50有反应
+#ros@ros:~$ adb shell input swipe 2335 1  2300 50 50
+
+def sildes(x, y, x1, y1, h):
+    os.system('adb shell input swipe {} {} {} {} {}'.format(x, y, x1, y1, h))
+    
+
+#判断中心点在不在矩形内  
 class MinimalSubscriber(Node):
+    # 1168x 540 中心点 
+    #"[0.92,0.71,0.98,0.73]" 给的是百分比
+    #TODO 改成 0.5 0.5为百分比 中心点 更好一些 给出瞄准心的百分比坐标
+    def point_inside_rect(self, data):
+        d = data[1:-1].split(",")
+        rect = [2336 * float(d[0]), 1080 * float(d[1]), 2336 * float(d[2]), 1080 * float(d[3])]
+        print(rect)
+        if (rect[0] < 1168 < rect[2]) and (rect[1] < 540 < rect[3]):
+            print("ready go")
+            return TRUE
+        print("no ready!  touch screen!")
+        return False
 
     def __init__(self):
         super().__init__('minimal_subscriber')
@@ -31,6 +58,13 @@ class MinimalSubscriber(Node):
 
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
+        
+        
+        #判断中心点 是否在矩形内
+        self.point_inside_rect(msg.data)
+        
+        
+        
 
 
 def main(args=None):
